761dfc47b5346ea331ec82ce91a33c3007c77cc3
EAJB1 <edbuckett11@gmail.com>
2024-08-17 21:49:08 +0100

    Camera bounds system

--- Assets/Scripts/Lava.cs
+++ Assets/Scripts/Lava.cs
@@ -22,7 +22,6 @@ public class Lava : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision);
         if (collision.gameObject.CompareTag("Player"))
         {
             Player.instance.Die();
