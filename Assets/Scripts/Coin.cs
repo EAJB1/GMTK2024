a5870623a9c065fe555b37f9e2e0401e22c7a008
EAJB1 <edbuckett11@gmail.com>
2024-08-18 14:57:29 +0100

    Added Coin UI

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,7 +6,14 @@ public class Coin : MonoBehaviour
 {
     public static int coinsCollected = 0;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    CoinUI coinUI;
+
+    void Awake()
+    {
+        coinUI = FindObjectOfType<CoinUI>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject);
         if(collision.gameObject == Player.instance.gameObject)
@@ -19,10 +26,11 @@ public class Coin : MonoBehaviour
     public void Collect()
     {
         coinsCollected++;
-        Debug.Log("COINS: " + coinsCollected);
 
         //Play coin sfx
         SoundManager.instance.PlaySound("Coin");
+
+        coinUI.UpdateCoinUI(coinsCollected);
 
         Destroy(gameObject);
     }
